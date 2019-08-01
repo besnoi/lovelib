f189e20273077da67e06a93f270de4e8f1a6ae70
Neer <49652823+YoungNeer@users.noreply.github.com>
2019-08-01 18:44:41 +0530

    Add files via upload

--- Anima/Anima.lua
+++ Anima/Anima.lua
@@ -7,7 +7,7 @@
 local Anima={}
 --note that animOver(private) is for internal usage and animEnd(public) is for external usage
 function Anima:init()
-    local obj={stage=nil,actualText="",typeFunc=function() end,typeTimer=nil,typeSpeed=1,currentText="",rforever={x=true,y=true,r=true,sx=true,sy=true,op=true},mark={x=0,y=0,r=0,sx=0,sy=0,op=0},key={x=0,y=0,r=0,sx=0,sy=0,op=0},x=0,y=0,r=0,sx=0,sy=0,op=0,animStart=false,animOver=false,animEnd=false}
+    local obj={stage=nil,actualText="",typeFunc=function() end,typeTimer=nil,typeSpeed=1,currentText="",rforever={x=false,y=false,r=false,sx=false,sy=false,op=false},speed={x=1,y=1,r=math.rad(10),sx=0.01,sy=0.01,op=0.01},mark={x=0,y=0,r=0,sx=0,sy=0,op=0},key={x=0,y=0,r=0,sx=0,sy=0,op=0},current={x=0,y=0,r=0,sx=0,sy=0,op=0},animStart=false,animOver=false,animEnd=false}
     setmetatable(obj,self)
     self.__index=self
     return obj
@@ -48,14 +48,10 @@ function Anima:setRepeatRotation(r)
 end
 
 function Anima:newAnimation(animation,...)
+
     local width,height,mark;
-    if animation=='cscale' then
-        self.key.sx,self.key.sy,width,height,mark=...
-        assert(width and height,"Width and Height cannot be nil. Please provide a value")
-        self:newAnimation('scale',self.key.sx,self.key.sy,mark)
-        self:newAnimation('move',-width*self.key.sx/2,-height*self.key.sy/2,mark)
-        return
-    elseif animation=='scale' then
+
+    if animation=='scale' then
         self.key.sx,self.key.sy,mark=...
     elseif animation=='move' then
         self.key.x,self.key.y,mark=...
@@ -64,8 +60,9 @@ function Anima:newAnimation(animation,...)
     elseif animation=='opacity' then
         self.key.op,mark=...
     else
-        self.key.x,self.key.y,self.key.r,self.key.sx,self.key.sy,self.op,mark=animation,...
+        self.key.x,self.key.y,self.key.r,self.key.sx,self.key.sy,self.current.op,mark=animation,...
     end
+
     self.key.sx=self.key.sx or 0
     self.key.sy=self.key.sy or 0
     self.key.x=self.key.x or 0
@@ -76,6 +73,7 @@ function Anima:newAnimation(animation,...)
     if not mark then
         self.mark={x=0,y=0,r=0,sx=0,sy=0,op=0}
     end
+
 end
 
 function Anima:startNewAnimation(...)
@@ -86,70 +84,85 @@ end
 --useful if you want to roll back animation
 function Anima:animationRollBack(operation,mark)
     if operation=='move' then
-        self:newAnimation('move',-self.x,-self.y,mark)
+        self:newAnimation('move',-self.current.x,-self.current.y,mark)
     elseif operation=='rotate' then
-        self:newAnimation('rotate',-self.r,mark)
+        self:newAnimation('rotate',-self.current.r,mark)
     elseif operation=='scale' then
-        self:newAnimation('scale',-self.sx,-self.sy,mark)
+        self:newAnimation('scale',-self.current.sx,-self.current.sy,mark)
     elseif operation=='opacity' then
-        self:newAnimation('opacity',-self.op,mark)
+        self:newAnimation('opacity',-self.current.op,mark)
     else
-        self:newAnimation(-self.x,-self.y,-self.r,-self.sx,-self.sy,-self.op,mark)
+        self:newAnimation(-self.current.x,-self.current.y,-self.current.r,-self.current.sx,-self.current.sy,-self.current.op,mark)
     end
 end
+
+--starts animation, given arguments will give the animation a headstart
 function Anima:animationStart(x,y,r,sx,sy,op)
-    self.x,self.y=x or 0,y or 0
-    self.r=r or 0
-    self.sx,self.sy=sx or 0,sy or 0
-    self.op=op or 0
+    self.current.x,self.current.y=x or 0,y or 0
+    self.current.r=r or 0
+    self.current.sx,self.current.sy=sx or 0,sy or 0
+    self.current.op=op or 0
     self.animStart,self.animOver=true,false
     self.animEnd=false
 end
+
 --instead of resetting the values like in animationStart(), animationStartOver() uses the default values
+--useful when you have more than 1 animations and you don't want to restart the animation but proceed from where you are
+
 function Anima:animationStartOver(x,y,r,sx,sy,op)
-    self.x,self.y=x or self.x,y or self.y
-    self.r=r or self.r
-    self.sx,self.sy=sx or self.sx,sy or self.sy
-    self.op=op or self.op
+    self.current.x,self.current.y=x or self.current.x,y or self.current.y
+    self.current.r=r or self.current.r
+    self.current.sx,self.current.sy=sx or self.current.sx,sy or self.current.sy
+    self.current.op=op or self.current.op
     self.animStart,self.animOver=true,false
     self.animEnd=false
 end
+
 function Anima:animationStarted()
     return self.animStart
 end
+
 function Anima:animationRunning()
     return not self.animEnd
 end
+
 function Anima:animationOver()
     return self.animEnd
 end
+
 function Anima:animationStop(arg)
     if arg==nil then arg=true end
     self.animEnd=arg
 end
+
 function Anima:resetAnimation()
-    self.x,self.y,self.r,self.sx,self.sy,self.op=0,0,0,0,0,0
+    self.current.x,self.current.y,self.current.r,self.current.sx,self.current.sy,self.current.op=0,0,0,0,0,0
 end
+
 function Anima:resetKey()
     self.key={x=0,y=0,r=0,sx=0,sy=0,op=0}
 end
+
 function Anima:resetKeyM()
     self.key.x=0
     self.key.y=0
 end
+
 function Anima:resetKeyS()
     self.key.sx=0
     self.key.sy=0
 end
+
 function Anima:resetKeyO()
     self.key.op=0
 end
+
 --Many times you want to mark a position for various reasons - maybe you want to continue
 --from where you stopped and you can do this on your own but I'd discourage you 'cause you 
 --are only wasting your time since it has already been done for you in animationMark
 function Anima:animationMark(x,y,r,sx,sy,op)
     if x==nil then
-        self.mark={x=self.mark.x+self.x,y=self.mark.y+self.y,r=self.mark.r+self.r,sx=self.mark.sx+self.sx,sy=self.mark.sy+self.sy,op=self.mark.op+self.op}
+        self.mark={x=self.mark.x+self.current.x,y=self.mark.y+self.current.y,r=self.mark.r+self.current.r,sx=self.mark.sx+self.current.sx,sy=self.mark.sy+self.current.sy,op=self.mark.op+self.current.op}
     else
         self.mark.x=self.mark.x+x
         self.mark.y=self.mark.y+(y or 0)
@@ -159,21 +172,28 @@ function Anima:animationMark(x,y,r,sx,sy,op)
         self.mark.op=self.mark.op+(op or 0)
     end
 end
+
+--marks the destination - you'll do it quite often when staging
 function Anima:animationMarkKey()
     self.mark=self.key
 end
+
 function Anima:markM()
-    self:animationMark(self.x,self.y,0)
+    self:animationMark(self.current.x,self.current.y,0)
 end
+
 function Anima:markR()
-    self:animationMark(0,0,self.r)
+    self:animationMark(0,0,self.current.r)
 end
+
 function Anima:markS()
-    self:animationMark(0,0,0,self.sx,self.sy)
+    self:animationMark(0,0,0,self.current.sx,self.current.sy)
 end
+
 function Anima:markO()
-    self:animationMark(0,0,0,0,0,self.op)
+    self:animationMark(0,0,0,0,0,self.current.op)
 end
+
 function Anima:updateFromTable(animtbl,cmts,funcs)
     for i in ipairs(animtbl) do
         --think of i as the self.stage
@@ -194,7 +214,7 @@ function Anima:updateFromTable(animtbl,cmts,funcs)
             self:animationStart()
             self.stage=self.stage+1
         end
-    end    
+    end
     if cmts and (not funcs) then
         assert(type(cmts)=='table',"Oops! Table expected in #2nd argument, got '"..type(cmts).."''")
         self:update(cmts['dx'],cmts['dy'],cmts['dr'],cmts['dsx'],cmts['dsy'],cmts['dop'],cmts['f']) 
@@ -203,15 +223,39 @@ function Anima:updateFromTable(animtbl,cmts,funcs)
     end
 end
 
-function Anima:updateT(dt)
-    self:update(nil,nil,nil,nil,nil,nil,nil,dt)
+function Anima:setMoveSpeed(dx,dy)
+    self.speed.x,self.speed.y=dx or 1,dy or 1
 end
 
-function Anima:update(stepx,stepy,stepr,stepsx,stepsy,stepop,forever,dt)
-    --(self.x<self.key.x and 1 or -1)
-    --(self.y>self.key.y and 1 or -1)
+function Anima:setRotationSpeed(dr)
+    self.speed.r=dr or math.rad(10)
+end
 
-    if self.typeTimer then 
+function Anima:setScaleSpeed(dsx,dsy)
+    self.speed.sx,self.speed.sy=dsx or 0.01,dsy or 0.01
+end
+
+function Anima:setOpacitySpeed(op)
+    self.speed.op=op or 0.01
+end
+
+function Anima:setSpeed(dx,dy,dr,dsx,dsy,dop)
+    self:setMoveSpeed(dx,dy)
+    self:setRotationSpeed(dr)
+    self:setScaleSpeed(dsx,dsy)
+    self:setOpacitySpeed(dop)
+end
+
+function Anima:setTypingSpeed(speed)
+    self.typeSpeed=speed or 1
+end
+
+function Anima:update(dt)
+
+    --(self.current.x<self.key.x and 1 or -1)
+    --(self.current.y>self.key.y and 1 or -1)
+
+    if self.typeTimer and self.animStart then 
         self.typeTimer=self.typeTimer+dt
         if self.typeTimer*self.typeSpeed>1 then
             self.currentText=self.currentText..self.actualText:sub(1,1)
@@ -225,40 +269,21 @@ function Anima:update(stepx,stepy,stepr,stepsx,stepsy,stepop,forever,dt)
         self:updateFromTable(stepx,stepy,stepr)
         return
     end
-    stepx=stepx or 1
-    stepy=stepy or 1
-    stepr=stepr or math.pi/20
-    stepsx=stepsx or 0.01
-    stepsy=stepsy or 0.01
-    stepop=stepop or 0.01
+
     if self.animStart and not self.animOver then
-        if self.key.x~=0 and self.key.x~=self.x then self.x=self.x+(self.x<self.key.x and stepx or -stepx) end
-        if self.key.y~=0 and self.key.y~=self.y then self.y=self.y+(self.y<self.key.y and stepy or -stepy) end
-        if self.key.sx~=0 and self.key.sx~=self.sx then self.sx=self.sx+(self.sx<self.key.sx and stepsx or -stepsx) end
-        if self.key.sy~=0 and self.key.sy~=self.sy then self.sy=self.sy+(self.sy<self.key.sy and stepsy or -stepsy) end
-        if self.key.r~=0 and self.key.r~=self.r then self.r=self.r+(self.r<self.key.r and stepr or -stepr) end
-        if self.key.op~=0 and self.key.op~=self.op then self.op=self.op+(self.op<self.key.op and stepop or -stepop) end
-        if (self.key.x>0 and self.x>self.key.x) or (self.key.x<0 and self.x<self.key.x) then
-            self.x=self.key.x
+
+        for k in pairs(self.key) do 
+            if self.key[k]~=0 and self.key[k]~=self.current[k] then
+                self.current[k]=self.current[k]+(self.current[k]<self.key[k] and self.speed[k] or -self.speed[k])
+            end
+            if (self.key[k]>0 and self.current[k]>self.key[k]) or (self.key[k]<0 and self.current[k]<self.key[k]) then
+                self.current[k]=self.key[k]
+            end    
         end
-        if (self.key.sx>0 and self.sx>self.key.sx) or (self.key.sx<=0 and self.sx<self.key.sx) then
-            self.sx=self.key.sx
-        end
-        if (self.key.y>0 and self.y>self.key.y) or (self.key.y<=0 and self.y<self.key.y) then
-            self.y=self.key.y
-        end
-        if (self.key.sy>0 and self.sy>self.key.sy) or (self.key.sy<=0 and self.sy<self.key.sy) then
-            self.sy=self.key.sy
-        end
-        if (self.key.r>0 and self.r>self.key.r) or (self.key.r<=0 and self.r<self.key.r) then
-            self.r=self.key.r
-        end
-        if (self.key.op>0 and self.op>self.key.op) or (self.key.op<=0 and self.op<self.key.op) then
-            self.op=self.key.op
-        end
-        if self.x==self.key.x and self.y==self.key.y and self.r==self.key.r and self.sx==self.key.sx and self.sy==self.key.sy and self.op==self.key.op and self.actualText:len()==0 then
+
+        if self.current.x==self.key.x and self.current.y==self.key.y and self.current.r==self.key.r and self.current.sx==self.key.sx and self.current.sy==self.key.sy and self.current.op==self.key.op and self.actualText:len()==0 then
         
-            if forever then
+            if self.rforever.x or self.rforever.y or self.rforever.r or self.rforever.sx or self.rforever.sy or self.rforever.op then
                 if self.rforever.x then self.key.x=-self.key.x end
                 if self.rforever.y then self.key.y=-self.key.y end
                 if self.rforever.r then self.key.r=-self.key.r end
@@ -273,30 +298,15 @@ function Anima:update(stepx,stepy,stepr,stepsx,stepsy,stepop,forever,dt)
         end
     end
 end
-function Anima:updateF(x,y,r,sx,sy,op)
-    self:update(x or 1,y or 1,r or math.pi/20,sx or 0.01,sy or 0.01,op or 0.01,true)
-end
-function Anima:updateM(stepx,stepy)
-    self:update(stepx,stepy)
-end
 
-function Anima:updateR(stepr,forever)
-    self:update(1,1,stepr,forever)
-end
-
-function Anima:updateS(stepsx,stepsy,forever)
-    self:update(1,1,math.pi/20,stepsx,stepsy,forever)
-end
-
-function Anima:updateO(stepo,forever)
-    self:update(1,1,math.pi/20,0.01,0.01,stepo,forever)
-end
 --render the animation
 function Anima:render(img,x,y,r,sx,sy,addKey,...)
+
     --you may want - sometimes - to have the render before the animation part look just like
-    --how it would look after animation for that addKey must be true
+    --how it would look after animation. For that addKey must be true
     --P.S. FOR MORE CONTROL I RECOMMEND YOU USE animationOver() AND RENDER IN TWO DIFFERENT WAYS
     --assert(img,"The 'render' function requires atleast a drawable object as an argument")
+
     x=(x or 0) + (addKey==true and self.key.x or 0)
     y=(y or 0) + (addKey==true and self.key.y or 0)
     r=(r or 0) + (addKey==true and self.key.r or 0)
@@ -304,75 +314,87 @@ function Anima:render(img,x,y,r,sx,sy,addKey,...)
     sy=(sy or 1) + (addKey==true and self.key.sy or 0 )
     if addKey~=true and addKey then args=addKey end
     local cr,cg,cb,ca=love.graphics.getColor()
-    love.graphics.setColor(cr,cg,cb,ca+self.op+(addKey and self.key.op or 0))
+    love.graphics.setColor(cr,cg,cb,ca+self.current.op+(addKey and self.key.op or 0))
     
-    love.graphics.draw(img,x+self.mark.x+self.x,y+self.mark.y+self.y,r+self.mark.r+self.r,sx+self.mark.sx+self.sx,sy+self.mark.sy+self.sy,args,...)
+    love.graphics.draw(img,x+self.mark.x+self.current.x,y+self.mark.y+self.current.y,r+self.mark.r+self.current.r,sx+self.mark.sx+self.current.sx,sy+self.mark.sy+self.current.sy,args,...)
     love.graphics.setColor(cr,cg,cb,ca)
 end
 
---i could have done this automatically in render function, but it would have been slower
-
-function Anima:print(text,x,y,r,sx,sy,addKey,...)
+function Anima:printKey(text,x,y,r,sx,sy,addKey,...)
     x=(x or 0) + (addKey==true and self.key.x or 0)
     y=(y or 0) + (addKey==true and self.key.y or 0)
     r=(r or 0) + (addKey==true and self.key.r or 0)
     sx=(sx or 1) + (addKey==true and self.key.sx or 0)
     sy=(sy or 1) + (addKey==true and self.key.sy or 0 )
-    if addKey~=true and addKey then args=addKey end
     local cr,cg,cb,ca=love.graphics.getColor()
-    love.graphics.setColor(cr,cg,cb,ca+self.op+(addKey and self.key.op or 0))
+    love.graphics.setColor(cr,cg,cb,ca+self.current.op+(addKey and self.key.op or 0))
     
-    love.graphics.print(text,x+self.mark.x+self.x,y+self.mark.y+self.y,r+self.mark.r+self.r,sx+self.mark.sx+self.sx,sy+self.mark.sy+self.sy,args,...)
+    love.graphics.print(text,x+self.mark.x+self.current.x,y+self.mark.y+self.current.y,r+self.mark.r+self.current.r,sx+self.mark.sx+self.current.sx,sy+self.mark.sy+self.current.sy,...)
+    love.graphics.setColor(cr,cg,cb,ca)
+end
+
+function Anima:print(text,x,y,r,sx,sy,...)
+    self:printKey(text,x,y,r,sx,sy,false,...)
+end
+
+function Anima:printfKey(text,x,y,limit,alignmode,r,sx,sy,addKey,...)
+    x=(x or 0) + (addKey==true and self.key.x or 0)
+    y=(y or 0) + (addKey==true and self.key.y or 0)
+    r=(r or 0) + (addKey==true and self.key.r or 0)
+    sx=(sx or 1) + (addKey==true and self.key.sx or 0)
+    sy=(sy or 1) + (addKey==true and self.key.sy or 0 )
+    local cr,cg,cb,ca=love.graphics.getColor()
+    love.graphics.setColor(cr,cg,cb,ca+self.current.op+(addKey and self.key.op or 0))
+    
+    love.graphics.printf(text,x+self.mark.x+self.current.x,y+self.mark.y+self.current.y,limit,alignmode,r+self.mark.r+self.current.r,sx+self.mark.sx+self.current.sx,sy+self.mark.sy+self.current.sy,...)
     love.graphics.setColor(cr,cg,cb,ca)
 end
 
 function Anima:printf(text,x,y,limit,alignmode,r,sx,sy,addKey,...)
-    x=(x or 0) + (addKey==true and self.key.x or 0)
-    y=(y or 0) + (addKey==true and self.key.y or 0)
-    r=(r or 0) + (addKey==true and self.key.r or 0)
-    sx=(sx or 1) + (addKey==true and self.key.sx or 0)
-    sy=(sy or 1) + (addKey==true and self.key.sy or 0 )
-    if addKey~=true and addKey then args=addKey end
-    local cr,cg,cb,ca=love.graphics.getColor()
-    love.graphics.setColor(cr,cg,cb,ca+self.op+(addKey and self.key.op or 0))
-    
-    love.graphics.printf(text,x+self.mark.x+self.x,y+self.mark.y+self.y,limit,alignmode,r+self.mark.r+self.r,sx+self.mark.sx+self.sx,sy+self.mark.sy+self.sy,args,...)
-    love.graphics.setColor(cr,cg,cb,ca)
+    self:printfKey(text,x,y,limit,alignmode,r,sx,sy,false,...)
 end
 
 function Anima:getX(x)
     x=x or 0
     --note x is the initial abscissa
-    return x+self.mark.x+self.x
+    return x+self.mark.x+self.current.x
 end
+
 function Anima:getY(y)
     y=y or 0
     --note y is the initial ordinate
-    return y+self.mark.y+self.y
+    return y+self.mark.y+self.current.y
 end
+
 function Anima:getSX(sx)
     sx=sx or 0
     --note sx is the initial scale along x axis
-    return sx+self.mark.sx+self.sx
+    return sx+self.mark.sx+self.current.sx
 end
+
 function Anima:getSY(sy)
     sy=sy or 0
     --note sy is the initial scale along y axis
-    return sy+self.mark.sy+self.sy
+    return sy+self.mark.sy+self.current.sy
 end
+
 function Anima:getPosition(x,y)
     --note x and y are the initial positions
     return self:getX(x),self:getY(y)
 end
+
 function Anima:getRotation(r)
     --note r is the initial angle of rotation
     r=r or 0
-    return r+self.mark.r+self.r
+    return r+self.mark.r+self.current.r
 end
+
 function Anima:getScale(sx,sy)
-     --note sx and sy are the scale values
+     --note sx and sy are the initial scale values
     return self:getSX(sx),self:getSY(sy)
 end
+
+--i could have done this automatically in render function, but it would have been slower
 function Anima:renderQuadKey(img,quad,x,y,r,sx,sy,addKey,...)
     x=(x or 0) + (addKey and self.key.x or 0)
     y=(y or 0) + (addKey and self.key.y or 0)
@@ -381,9 +403,9 @@ function Anima:renderQuadKey(img,quad,x,y,r,sx,sy,addKey,...)
     sy=(sy or 1) + (addKey and self.key.sy or 0 )
     
     local cr,cg,cb,ca=love.graphics.getColor()
-    love.graphics.setColor(cr,cg,cb,ca+self.op+(addKey and self.key.op or 0))
+    love.graphics.setColor(cr,cg,cb,ca+self.current.op+(addKey and self.key.op or 0))
 
-    love.graphics.draw(img,quad,x+self.mark.x+self.x,y+self.mark.y+self.y,r+self.mark.r+self.r,sx+self.mark.sx+self.sx,sy+self.mark.sy+self.sy,...)
+    love.graphics.draw(img,quad,x+self.mark.x+self.current.x,y+self.mark.y+self.current.y,r+self.mark.r+self.current.r,sx+self.mark.sx+self.current.sx,sy+self.mark.sy+self.current.sy,...)
     love.graphics.setColor(cr,cg,cb,ca)
 end
 
